9572225f280c1cf8a1884c89d6d1a64f5c76cf3c
tominen <tomasz.nedza@outlook.com>
2023-07-30 14:46:11 +0200

    loging baase

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,70 @@
-import { Box, Button, FormControl, FormLabel, Heading, Input, Stack } from '@chakra-ui/react'
-import React from 'react'
-import { PiBalloonFill } from "react-icons/pi"
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Spinner, Stack, Text, useToast } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { PiBalloonFill, PiPassword } from "react-icons/pi"
+import { Link } from 'react-router-dom';
 
 function LoginPage() {
+    const toast = useToast()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    const changeEmail = (e) => setEmail(e.target.value)
+    const changePassword = (e) => setPassword(e.target.value)
+
+    //console.log(email,password)
+
+    const loginUser = async () => {
+
+
+        if (!email || !password) {
+            setError(true)
+            return
+        }
+
+        setLoading(true)
+
+        try {
+
+            const response = await fetch("http://localhost:5000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                // zmiana JSONA na obiekt user (wysyłam do bazy)
+                //body: JSON.stringify(user)
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+
+            })
+            // zamiana respone na odpowiedz, którą moge odczytac JS
+            const data = await response.json()
+
+            toast({
+                title: data.message,
+                status: data.ok ? 'success' : 'error',
+                duration: 3000,
+                isClosable: true,
+            })
+
+        } catch (error) {
+            console.log(error)
+
+            toast({
+                title: 'Błąd serwera',
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+            })
+        } finally{
+            setError(false)
+        }
+        setLoading(false)
+    }
+
     return (
         <Box
             p={4}
@@ -15,8 +77,8 @@ function LoginPage() {
             flexDirection="column"
         >
 
-            <Heading 
-                display="flex" 
+            <Heading
+                display="flex"
                 alignItems="center"
                 fontSize="50px"
                 mb="30px"
@@ -27,17 +89,31 @@ function LoginPage() {
 
             <Stack spacing={3} w='100%'>
 
-                <FormControl>
+                <FormControl isInvalid={error}>
                     <FormLabel>Email</FormLabel>
-                    <Input placeholder='Podaj adres email' />
+                    <Input onChange={changeEmail} placeholder='Podaj adres email' />
+                    {error && <FormErrorMessage>To pole jest wymagane</FormErrorMessage>}
                 </FormControl>
 
-                <FormControl>
+                <FormControl isInvalid={error}>
                     <FormLabel>Hasło</FormLabel>
-                    <Input placeholder='Podaj swoje hasło' />
+                    <Input onChange={changePassword} placeholder='Podaj swoje hasło' />
+                    {error && <FormErrorMessage>To pole jest wymagane</FormErrorMessage>}
                 </FormControl>
 
-                <Button colorScheme='green' size='lg'>Zaloguj</Button>
+                <Button
+                    onClick={loginUser}
+                    colorScheme='green'
+                    size='lg'
+                >
+                    {loading ? <Spinner color='green.500' /> : 'Zaloguj'}
+                </Button>
+
+                <Text textAlign="center" mt="10px">
+                    Nie masz konta?
+                    <Link style={{ color: '#38A169' }} to="/register">Zarejestruj się</Link>
+                </Text>
+
             </Stack>
         </Box>
     )
